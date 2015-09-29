48e9af679531e5dd961c34ec1e8532247417526b
TOMalley104 <tom@flatironschool.com>
2015-09-29 15:03:54 -0400

    fix appdelegate lack-of-VC crash and update Specta

--- CaesarCipher/FISAppDelegate.m
+++ CaesarCipher/FISAppDelegate.m
@@ -14,7 +14,8 @@
 {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
-    self.window.backgroundColor = [UIColor whiteColor];
+    self.window.backgroundColor = [UIColor blackColor];
+    [self.window setRootViewController:[[UIViewController alloc] init]];
     [self.window makeKeyAndVisible];
     
     return YES;
